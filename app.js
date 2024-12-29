61c6adcb68191d86a62accfb887b6a966721d288
jemsrr <jemsrakholiya4949@gmail.com>
2024-12-29 23:22:46 +0530

    small fix

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const schemas = require("./schemas");
 const resolvers = require("./resolvers");
 const { applyMiddleware } = require("graphql-middleware");
 const authMiddleware = require("./Middleware/auth.middleware");
+const cors = require("cors");
 
 const schema = makeExecutableSchema({
   typeDefs: schemas,
@@ -26,6 +27,12 @@ const middlewareSchema = applyMiddleware(schema, {
 module.exports = middlewareSchema;
 
 const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 const startServer = async () => {
   const server = new ApolloServer({
